530a23abe5845a0048ed393457e7cd05e188569b
Gihozo23 <christellegihozo23@gmail.com>
2024-10-10 11:53:14 +0200

    refactor: use http handler to handle requests through routing

--- src/lessons/lessons.controller.ts
+++ src/lessons/lessons.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('lessons')
-export class LessonsController {}
+export class LessonsController {
+  @Get('math')
+  findAllLessons() {
+    return 'This action returns all lessons';
+  }
+  @Get('/:id')
+  findOneLesson(@Param('id') id: string) {
+    return `This action returns a #${id} lesson`;
+  }
+}
