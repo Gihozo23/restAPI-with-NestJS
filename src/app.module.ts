10cc2bae4e20870297c6cd8e4b22796bd039af34
Gihozo23 <christellegihozo23@gmail.com>
2024-10-14 17:10:36 +0200

    feat: add another controller for the exercises

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LessonsController } from './lessons/lessons.controller';
+import { ExercisesController } from './exercises/exercises.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, LessonsController],
+  controllers: [AppController, LessonsController, ExercisesController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/lessons/lessons.controller.ts
+++ src/lessons/lessons.controller.ts
@@ -1,17 +1,33 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('lessons')
 export class LessonsController {
-  @Get('math')
+  @Get()
   findAllLessons() {
     return 'This action returns all lessons';
   }
   @Get('/:id')
   findOneLesson(@Param('id') id: string) {
-    return `This action returns a #${id} lesson`;
+    if (id) {
+      return `This action returns a #${id} lesson`;
+    } else return 'not found';
   }
   @Post()
   createLesson(@Body('name') name: string) {
-    return `${name} lesson has been created`;
+    `${name} lesson has been created`;
+  }
+
+  @Patch('/:id')
+  updateLesson(@Param('id') id: string, @Body() body) {
+    return `This action updated the #${id}`;
   }
 }
